9f519f29472eb5769671dffcef72be7f10f1ed3e
sanalgithub <sanalkumar8065@gmail.com>
2026-09-09 18:21:16 +0530

    feat: add express app setup with cors and error handling

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Real-Time Order Analytics Backend is running 🚀");
+});
+
+
+
+
+export default app;
